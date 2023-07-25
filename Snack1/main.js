f9164b6c41e8517000a6bdcfd39c7f69209e75f5
AlessioMontebello90 <alessio.montebello.am@gmail.com>
2023-07-25 18:00:33 +0200

    feat JS and HTML snack1

--- Snack1/main.js
+++ Snack1/main.js
@@ -1,3 +1,6 @@
 //  propongo un promt dove chiedo un numero all'utente
+
+let userNumber = prompt("Inserisci un numero");
+
 // seleziono elemento
 // imposto una condizione doce SE il numero generato è pari viene stampato su pagina html con ID
