89067aedf17dad0616eb1a75e3f83c9480ecca57
AlessioMontebello90 <alessio.montebello.am@gmail.com>
2023-07-25 18:32:07 +0200

    add array Snack2

---
+++ Snack2/main.js
@@ -0,0 +1,28 @@
+// creo due array con lo stesso numero di contenuti (lista invitati)
+const names = [
+  "Guè",
+  "Salmo",
+  "Sfera",
+  "Rkomi",
+  "Kendrik",
+  "Kobe",
+  "Shaq",
+  "James",
+  "Anna",
+  "Raty",
+];
+const surnames = [
+  "Blu",
+  "Rosso",
+  "Verde",
+  "Arancione",
+  "Rosa",
+  "Giallo",
+  "Melo",
+  "Rusty",
+  "Ernia",
+  "Swag",
+];
+
+// scrivo due variabili che producano un numero randomico
+// genero e imposto un ciclo nel quale *Per Ogni* elemento dell'array stampa un elemento per array con indice popolato da variabile randomica creata prima
