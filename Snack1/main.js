98cf55271244c1fb14a415832fb271dbdc9ec7ba
AlessioMontebello90 <alessio.montebello.am@gmail.com>
2023-07-25 18:10:13 +0200

    fix JS and HTML files

--- Snack1/main.js
+++ Snack1/main.js
@@ -3,4 +3,15 @@
 let userNumber = prompt("Inserisci un numero");
 
 // seleziono elemento
+
+const yourNumber = document.getElementById("your-number");
+
 // imposto una condizione doce SE il numero generato è pari viene stampato su pagina html con ID
+
+if (userNumber % 2 == 0) {
+  yourNumber.innerHTML += userNumber;
+} else {
+  userNumber++;
+
+  yourNumber.innerHTML += userNumber;
+}
